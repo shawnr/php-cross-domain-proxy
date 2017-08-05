9810a45cfb8e7cd3833ff9e24a938d3c324446c0
Shawn Rider <shawn@shawnrider.com>
2017-08-05 09:56:29 -0700

    Updated index.html.
    Change-Id: I33fb76eb0452c2376b1e090f6e18d93ff6fadd67

--- proxy.php
+++ proxy.php
@@ -136,24 +136,18 @@ if (CSAJAX_FILTERS) {
 if ($request_method == 'GET' && count($request_params) > 0 && (!array_key_exists('query', $p_request_url) || empty($p_request_url['query']))) {
     if (count($request_params) > 0){
         $request_url = $request_url . '&' . http_build_query($request_params);
-    } else {
-        $request_url .= '?' . http_build_query($request_params);
     }
-
 
     //echo "Request_url: " . $request_url;
 } else {
-    echo "this check failed";
+    //echo "this check failed";
     if (count($request_params) > 0){
         $request_url = $request_url . '&' . http_build_query($request_params);
     }
-    echo "Request_url: " . $request_url;
+    //echo "Request_url: " . $request_url;
 }
 
-if (substr($request_url, -1) == "?") {
-    //$request_url = rtrim(trim($request_url), "?");
-}
-echo "Outer Request_url: " . $request_url;
+//echo "Outer Request_url: " . $request_url;
 // let the request begin
 $ch = curl_init($request_url);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);   // (re-)send headers
